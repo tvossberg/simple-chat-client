ef81903a04eae9d8dfd09c7bb85a0d023adea1ed
Trevor Vossberg <tvossber@gmail.com>
2019-05-04 23:06:39 -0500

    Get returned map into appropriate types

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,8 +3,8 @@ import com.google.gson.Gson;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Application {
 
@@ -13,5 +13,12 @@ public class Application {
 
         Map<String, ArrayList<Double>> map = gson.fromJson(new FileReader(args[0]), Map.class);
         System.out.println(map);
+
+        Map<Long, Set<Long>> realMap = new HashMap<>();
+        for(Map.Entry<String,ArrayList<Double>> entry: map.entrySet()){
+            Set<Long> valueSet= entry.getValue().stream().map(Double::longValue).collect(Collectors.toSet());
+            realMap.put(Long.parseLong(entry.getKey()),valueSet);
+        }
+        System.out.println(realMap);
     }
 }
